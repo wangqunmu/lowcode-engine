6114dac15019f0bd4ab6e0bd5b433fa2202d7134
wangqun <>
2024-01-10 16:06:59 +0800

    格式化

--- packages/designer/src/designer/designer-view.tsx
+++ packages/designer/src/designer/designer-view.tsx
@@ -7,60 +7,60 @@ import './designer.less';
 import { clipboard } from './clipboard';
 
 type IProps = DesignerProps & {
-  designer?: Designer;
+    designer?: Designer;
 };
 
 export class DesignerView extends Component<IProps> {
-  readonly designer: Designer;
-  readonly viewName: string | undefined;
+    readonly designer: Designer;
+    readonly viewName: string | undefined;
 
-  constructor(props: IProps) {
-    super(props);
-    const { designer, ...designerProps } = props;
-    this.viewName = designer?.viewName;
-    if (designer) {
-      this.designer = designer;
-      designer.setProps(designerProps);
-    } else {
-      this.designer = new Designer(designerProps);
+    constructor(props: IProps) {
+        super(props);
+        const { designer, ...designerProps } = props;
+        this.viewName = designer?.viewName;
+        if (designer) {
+            this.designer = designer;
+            designer.setProps(designerProps);
+        } else {
+            this.designer = new Designer(designerProps);
+        }
     }
-  }
 
-  shouldComponentUpdate(nextProps: DesignerProps) {
-    this.designer.setProps(nextProps);
-    const { props } = this;
-    if (
-      nextProps.className !== props.className ||
-      nextProps.style !== props.style ||
-      nextProps.dragGhostComponent !== props.dragGhostComponent
-    ) {
-      return true;
+    shouldComponentUpdate(nextProps: DesignerProps) {
+        this.designer.setProps(nextProps);
+        const { props } = this;
+        if (
+            nextProps.className !== props.className ||
+            nextProps.style !== props.style ||
+            nextProps.dragGhostComponent !== props.dragGhostComponent
+        ) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  componentDidMount() {
-    const { onMount } = this.props;
-    if (onMount) {
-      onMount(this.designer);
+    componentDidMount() {
+        const { onMount } = this.props;
+        if (onMount) {
+            onMount(this.designer);
+        }
+        clipboard.injectCopyPaster(document);
+        this.designer.postEvent('mount', this.designer);
     }
-    clipboard.injectCopyPaster(document);
-    this.designer.postEvent('mount', this.designer);
-  }
 
-  UNSAFE_componentWillMount() {
-    this.designer.purge();
-  }
+    UNSAFE_componentWillMount() {
+        this.designer.purge();
+    }
 
-  render() {
-    const { className, style, dragGhostComponent } = this.props;
-    const DragGhost = dragGhostComponent || BuiltinDragGhostComponent;
+    render() {
+        const { className, style, dragGhostComponent } = this.props;
+        const DragGhost = dragGhostComponent || BuiltinDragGhostComponent;
 
-    return (
-      <div className={classNames('lc-designer', className)} style={style}>
-        <DragGhost designer={this.designer} />
-        <ProjectView designer={this.designer} />
-      </div>
-    );
-  }
+        return (
+            <div className={classNames('lc-designer', className)} style={style}>
+                <DragGhost designer={this.designer} />
+                <ProjectView designer={this.designer} />
+            </div>
+        );
+    }
 }
