1a547aa9678fe204f1a577a4ca99cefe9784b677
wangqun <>
2024-01-11 14:48:26 +0800

    格式化

--- packages/editor-skeleton/src/layouts/workbench.tsx
+++ packages/editor-skeleton/src/layouts/workbench.tsx
@@ -16,42 +16,42 @@ import { EditorConfig, PluginClassSet } from '@alilc/lowcode-types';
 
 @observer
 export class Workbench extends Component<{
-  skeleton: ISkeleton;
-  config?: EditorConfig;
-  components?: PluginClassSet;
-  className?: string;
-  topAreaItemClassName?: string;
+    skeleton: ISkeleton;
+    config?: EditorConfig;
+    components?: PluginClassSet;
+    className?: string;
+    topAreaItemClassName?: string;
 }> {
-  constructor(props: any) {
-    super(props);
-    const { config, components, skeleton } = this.props;
-    skeleton.buildFromConfig(config, components);
-  }
+    constructor(props: any) {
+        super(props);
+        const { config, components, skeleton } = this.props;
+        skeleton.buildFromConfig(config, components);
+    }
 
-  render() {
-    const {
-      skeleton,
-      className,
-      topAreaItemClassName,
-    } = this.props;
-    return (
-      <div className={classNames('lc-workbench', className)}>
-        <SkeletonContext.Provider value={this.props.skeleton}>
-          <TopArea area={skeleton.topArea} itemClassName={topAreaItemClassName} />
-          <div className="lc-workbench-body">
-            <LeftArea area={skeleton.leftArea} />
-            <LeftFloatPane area={skeleton.leftFloatArea} />
-            <LeftFixedPane area={skeleton.leftFixedArea} />
-            <div className="lc-workbench-center">
-              <Toolbar area={skeleton.toolbar} />
-              <MainArea area={skeleton.mainArea} />
-              <BottomArea area={skeleton.bottomArea} />
+    render() {
+        const {
+            skeleton,
+            className,
+            topAreaItemClassName,
+        } = this.props;
+        return (
+            <div className={classNames('lc-workbench', className)}>
+                <SkeletonContext.Provider value={this.props.skeleton}>
+                    <TopArea area={skeleton.topArea} itemClassName={topAreaItemClassName} />
+                    <div className="lc-workbench-body">
+                        <LeftArea area={skeleton.leftArea} />
+                        <LeftFloatPane area={skeleton.leftFloatArea} />
+                        <LeftFixedPane area={skeleton.leftFixedArea} />
+                        <div className="lc-workbench-center">
+                            <Toolbar area={skeleton.toolbar} />
+                            <MainArea area={skeleton.mainArea} />
+                            <BottomArea area={skeleton.bottomArea} />
+                        </div>
+                        <RightArea area={skeleton.rightArea} />
+                    </div>
+                    <TipContainer />
+                </SkeletonContext.Provider>
             </div>
-            <RightArea area={skeleton.rightArea} />
-          </div>
-          <TipContainer />
-        </SkeletonContext.Provider>
-      </div>
-    );
-  }
+        );
+    }
 }
