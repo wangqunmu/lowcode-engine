19915a63bf36acd10bef79ee25a5635de86e831e
wangqun <>
2024-01-11 17:55:08 +0800

    格式化

--- packages/designer/src/designer/active-tracker.ts
+++ packages/designer/src/designer/active-tracker.ts
@@ -1,61 +1,61 @@
 import { INode } from '../document/node/node';
 import { obx, IEventBus, createModuleEventBus } from '@alilc/lowcode-editor-core';
 import {
-  IPublicTypeActiveTarget,
-  IPublicModelActiveTracker,
+    IPublicTypeActiveTarget,
+    IPublicModelActiveTracker,
 } from '@alilc/lowcode-types';
 import { isNode } from '@alilc/lowcode-utils';
 
-export interface IActiveTracker extends Omit< IPublicModelActiveTracker, 'track' | 'onChange' > {
-  _target: ActiveTarget | INode;
+export interface IActiveTracker extends Omit<IPublicModelActiveTracker, 'track' | 'onChange'> {
+    _target: ActiveTarget | INode;
 
-  track(originalTarget: ActiveTarget | INode): void;
+    track(originalTarget: ActiveTarget | INode): void;
 
-  onChange(fn: (target: ActiveTarget) => void): () => void;
+    onChange(fn: (target: ActiveTarget) => void): () => void;
 }
 
-export interface ActiveTarget extends Omit< IPublicTypeActiveTarget, 'node' > {
-  node: INode;
+export interface ActiveTarget extends Omit<IPublicTypeActiveTarget, 'node'> {
+    node: INode;
 }
 
 export class ActiveTracker implements IActiveTracker {
-  @obx.ref private _target?: ActiveTarget | INode;
+    @obx.ref private _target?: ActiveTarget | INode;
 
-  private emitter: IEventBus = createModuleEventBus('ActiveTracker');
+    private emitter: IEventBus = createModuleEventBus('ActiveTracker');
 
-  track(originalTarget: ActiveTarget | INode) {
-    let target = originalTarget;
-    if (isNode(originalTarget)) {
-      target = { node: originalTarget as INode };
+    track(originalTarget: ActiveTarget | INode) {
+        let target = originalTarget;
+        if (isNode(originalTarget)) {
+            target = { node: originalTarget as INode };
+        }
+        this._target = target;
+        this.emitter.emit('change', target);
     }
-    this._target = target;
-    this.emitter.emit('change', target);
-  }
 
-  get currentNode() {
-    return (this._target as ActiveTarget)?.node;
-  }
+    get currentNode() {
+        return (this._target as ActiveTarget)?.node;
+    }
 
-  get detail() {
-    return (this._target as ActiveTarget)?.detail;
-  }
+    get detail() {
+        return (this._target as ActiveTarget)?.detail;
+    }
 
-  /**
-   * @deprecated
-   */
-  /* istanbul ignore next */
-  get intance() {
-    return this.instance;
-  }
+    /**
+     * @deprecated
+     */
+    /* istanbul ignore next */
+    get intance() {
+        return this.instance;
+    }
 
-  get instance() {
-    return (this._target as ActiveTarget)?.instance;
-  }
+    get instance() {
+        return (this._target as ActiveTarget)?.instance;
+    }
 
-  onChange(fn: (target: ActiveTarget) => void): () => void {
-    this.emitter.addListener('change', fn);
-    return () => {
-      this.emitter.removeListener('change', fn);
-    };
-  }
+    onChange(fn: (target: ActiveTarget) => void): () => void {
+        this.emitter.addListener('change', fn);
+        return () => {
+            this.emitter.removeListener('change', fn);
+        };
+    }
 }
