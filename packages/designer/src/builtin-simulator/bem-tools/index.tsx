0dde64002b490d1e7815659ba2100b5a1c4b4f1e
wangqun <>
2024-01-11 12:32:37 +0800

    格式化

--- packages/designer/src/builtin-simulator/bem-tools/index.tsx
+++ packages/designer/src/builtin-simulator/bem-tools/index.tsx
@@ -11,27 +11,27 @@ import './borders.less';
 
 @observer
 export class BemTools extends Component<{ host: BuiltinSimulatorHost }> {
-  render() {
-    const { host } = this.props;
-    const { designMode } = host;
-    const { scrollX, scrollY, scale } = host.viewport;
-    if (designMode === 'live') {
-      return null;
-    }
-    return (
-      <div className="lc-bem-tools" style={{ transform: `translate(${-scrollX * scale}px,${-scrollY * scale}px)` }}>
-        { !engineConfig.get('disableDetecting') && <BorderDetecting key="hovering" host={host} /> }
-        <BorderSelecting key="selecting" host={host} />
-        { engineConfig.get('enableReactiveContainer') && <BorderContainer key="reactive-container-border" host={host} /> }
-        <InsertionView key="insertion" host={host} />
-        <BorderResizing key="resizing" host={host} />
-        {
-          host.designer.bemToolsManager.getAllBemTools().map(tools => {
-            const ToolsCls = tools.item;
-            return <ToolsCls key={tools.name} host={host} />;
-          })
+    render() {
+        const { host } = this.props;
+        const { designMode } = host;
+        const { scrollX, scrollY, scale } = host.viewport;
+        if (designMode === 'live') {
+            return null;
         }
-      </div>
-    );
-  }
+        return (
+            <div className="lc-bem-tools" style={{ transform: `translate(${-scrollX * scale}px,${-scrollY * scale}px)` }}>
+                {!engineConfig.get('disableDetecting') && <BorderDetecting key="hovering" host={host} />}
+                <BorderSelecting key="selecting" host={host} />
+                {engineConfig.get('enableReactiveContainer') && <BorderContainer key="reactive-container-border" host={host} />}
+                <InsertionView key="insertion" host={host} />
+                <BorderResizing key="resizing" host={host} />
+                {
+                    host.designer.bemToolsManager.getAllBemTools().map(tools => {
+                        const ToolsCls = tools.item;
+                        return <ToolsCls key={tools.name} host={host} />;
+                    })
+                }
+            </div>
+        );
+    }
 }
