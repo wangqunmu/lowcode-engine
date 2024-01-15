ed13fac2143dcd26d99f2b3c8feb19e5a9595f5d
wangqunmu <155887501+wangqunmu@users.noreply.github.com>
2024-01-15 18:30:00 +0800

    格式化

--- packages/react-simulator-renderer/src/builtin-components/leaf.tsx
+++ packages/react-simulator-renderer/src/builtin-components/leaf.tsx
@@ -1,24 +1,24 @@
 import { Component } from 'react';
 
 class Leaf extends Component {
-  static displayName = 'Leaf';
+    static displayName = 'Leaf';
 
-  static componentMetadata = {
-    componentName: 'Leaf',
-    configure: {
-      props: [{
-        name: 'children',
-        setter: 'StringSetter',
-      }],
-      // events/className/style/general/directives
-      supports: false,
-    },
-  };
+    static componentMetadata = {
+        componentName: 'Leaf',
+        configure: {
+            props: [{
+                name: 'children',
+                setter: 'StringSetter',
+            }],
+            // events/className/style/general/directives
+            supports: false,
+        },
+    };
 
-  render() {
-    const { children } = this.props;
-    return children;
-  }
+    render() {
+        const { children } = this.props;
+        return children;
+    }
 }
 
 export default Leaf;
